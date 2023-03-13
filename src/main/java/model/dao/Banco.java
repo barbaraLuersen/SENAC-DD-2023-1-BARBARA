70c06e2073818435c1edf9ef6a8492f1b93bfb07
barbara.lima1 <barbara.lima1@FLE207D04.alunos.sc.senac.br>
2023-03-13 11:26:16 -0300

    Alteração da conexão do banco de dados

--- src/main/java/model/dao/Banco.java
+++ src/main/java/model/dao/Banco.java
@@ -8,15 +8,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Banco {
-	
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; //MYSQL versao 8
-	//private static final String DRIVER = "com.mysql.jdbc.Driver"; //MYSQL versão 5
-	private static final String BANCODADOS = "dbfoodtruck";
-	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" + BANCODADOS;
+
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // MYSQL versao 8
+	// private static final String DRIVER = "com.mysql.jdbc.Driver"; //MYSQL versão
+	// 5
+	private static final String BANCODADOS = "exemplos";
+	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" + BANCODADOS
+			+ "?useTimezone=true&serverTimezone=UTC";
 	private static final String USER = "root";
-	private static final String PASSWORD = "";
-	
-	public static Connection getConnection(){
+	private static final String PASSWORD = "root";
+
+	public static Connection getConnection() {
 		try {
 			Connection conn = null;
 			Class.forName(DRIVER);
@@ -32,19 +34,19 @@ public class Banco {
 			return null;
 		}
 	}
-	
-	public static void closeConnection(Connection conn){
+
+	public static void closeConnection(Connection conn) {
 		try {
-			if(conn != null){
+			if (conn != null) {
 				conn.close();
 			}
 		} catch (SQLException e) {
 			System.out.println("Problema no fechamento da conexão.");
 			System.out.println("Erro: " + e.getMessage());
-		}	
+		}
 	}
-	
-	public static Statement getStatement(Connection conn){
+
+	public static Statement getStatement(Connection conn) {
 		try {
 			Statement stmt = conn.createStatement();
 			return stmt;
@@ -54,19 +56,19 @@ public class Banco {
 			return null;
 		}
 	}
-		
-	public static void closeStatement(Statement stmt){
+
+	public static void closeStatement(Statement stmt) {
 		try {
-			if(stmt != null){
+			if (stmt != null) {
 				stmt.close();
 			}
 		} catch (SQLException e) {
 			System.out.println("Problema no fechamento do Statement.");
 			System.out.println("Erro: " + e.getMessage());
-		}	
+		}
 	}
-	
-	public static PreparedStatement getPreparedStatement(Connection conn, String sql){
+
+	public static PreparedStatement getPreparedStatement(Connection conn, String sql) {
 		try {
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			return stmt;
@@ -76,8 +78,8 @@ public class Banco {
 			return null;
 		}
 	}
-	
-	public static PreparedStatement getPreparedStatementWithPk(Connection conn, String sql){
+
+	public static PreparedStatement getPreparedStatementWithPk(Connection conn, String sql) {
 		try {
 			PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			return stmt;
@@ -88,20 +90,20 @@ public class Banco {
 		}
 	}
 
-	public static void closePreparedStatement(Statement stmt){
+	public static void closePreparedStatement(Statement stmt) {
 		try {
-			if(stmt != null){
+			if (stmt != null) {
 				stmt.close();
 			}
 		} catch (SQLException e) {
 			System.out.println("Problema no fechamento do PreparedStatement.");
 			System.out.println("Erro: " + e.getMessage());
-		}	
+		}
 	}
-	
-	public static void closeResultSet(ResultSet result){
+
+	public static void closeResultSet(ResultSet result) {
 		try {
-			if(result != null){
+			if (result != null) {
 				result.close();
 			}
 		} catch (SQLException e) {
@@ -110,8 +112,3 @@ public class Banco {
 		}
 	}
 }
-
-
-
-
-
